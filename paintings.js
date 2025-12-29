71444ff4f2f24b29ffd68d9e9de6986f0fe0c477
Mismoooo <louegiebarle30@gmail.com>
2025-12-29 23:16:44 +0800

    Add files via upload

---
+++ paintings.js
@@ -0,0 +1,29 @@
+document.getElementById("mona-lisa").addEventListener("click", function (e) {
+  e.preventDefault();
+  window.location.href = "mona-lisa.html";
+});
+
+document.getElementById("girl-with-pearl-earrings").addEventListener("click", function (e) {
+  e.preventDefault();
+  window.location.href = "girl-with-pearl-earrings.html";
+});
+
+document.getElementById("arnolfini-portrait").addEventListener("click", function (e) {
+  e.preventDefault();
+  window.location.href = "arnolfini-portrait.html";
+});
+
+document.getElementById("the-starry-night").addEventListener("click", function (e) {
+  e.preventDefault();
+  window.location.href = "the-starry-night.html";
+});
+
+document.getElementById("liberty-leading-the-people").addEventListener("click", function (e) {
+  e.preventDefault();
+  window.location.href = "liberty-leading-the-people.html";
+});
+
+document.getElementById("the-garden-of-earthly-delights").addEventListener("click", function (e) {
+  e.preventDefault();
+  window.location.href = "the-garden-of-earthly-delights.html";
+});
